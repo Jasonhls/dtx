c9cb6c803c51731f3b40c99fb81f49a41f789e9f
Jasonhls <1597445096@qq.com>
2021-05-21 00:02:56 +0800

    提交

---
+++ dtx-seata-demo/dtx-seata-demo-bank1/src/main/java/com/cn/Bank1Application.java
@@ -0,0 +1,11 @@
+package com.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bank1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Bank1Application.class, args);
+    }
+}
